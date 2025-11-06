31449a1a64974b888cea53df2bc556951b86ce12
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-06 08:55:38 -0500

    slides added

--- src/components/products/slides-show/ProductSlide.tsx
+++ src/components/products/slides-show/ProductSlide.tsx
@@ -1,34 +1,78 @@
-"use client"
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
-import 'swiper/css';
+"use client";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import "swiper/css";
+import "./slideshow.css";
+import { Autoplay, FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Swiper as SwiperObject } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
 
 interface Props {
-    images:string[]
-    title:string
-    className?:string
+  images: string[];
+  title: string;
+  className?: string;
 }
 
-export const ProductSlide=( {images,title,className}:Props )=> {
-
+export const ProductSlide = ({ images, title, className }: Props) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperObject>();
 
   return (
-    <div className={className} >
+    <div className={className}>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        style={
+          {
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          } as React.CSSProperties
+        }
+        spaceBetween={10}
+        navigation={true}
+        autoplay={{
+            delay:2500
+        }}
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroy ? thumbsSwiper : null,
+        }}
+        modules={[FreeMode, Navigation, Thumbs, Autoplay]}
+        className="mySwiper2"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
+        {images.map((image) => (
+          <SwiperSlide key={image}>
+            <img
+              width={1024}
+              height={800}
+              src={`/products/${image}`}
+              alt={title}
+              className=" rounded-lg object-fill"
+            ></img>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image}>
+            <img
+              width={300}
+              height={300}
+              src={`/products/${image}`}
+              alt={title}
+              className=" rounded-lg object-fill"
+            ></img>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
-}
+};
